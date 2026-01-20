8f971a4e1f69ba228bb78290d5f83e9210372e25
katie-csp <baileyk7@csp.edu>
2026-01-19 21:54:55 -0600

    Pet.java

--- Pet.java
+++ Pet.java
@@ -1,3 +1,9 @@
+// Author: Katie Bailey.
+// Date: 01/19/2026
+// Class: CSC 422 100
+// Project: Assignment 1
+// File Name: Pet.java
+// Description: This file contains the code for the pet class.
 public class Pet {
     String name;
     int age;
@@ -6,4 +12,5 @@ public class Pet {
         this.name = name;
         this.age = age;
     }
+
 }
